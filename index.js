ad3b0a745ab8e4d53cda55cea0d930bb5174bd1b
Jamie Davis <davisjam@vt.edu>
2017-05-09 18:05:39 -0400

    A verbose version: comments, logging, etc.
    Help me understand

--- index.js
+++ index.js
@@ -7,37 +7,74 @@ module.exports = function (re, opts) {
     
     if (isRegExp(re)) re = re.source;
     else if (typeof re !== 'string') re = String(re);
+    console.log('re: ' + re);
     
-    try { re = parse(re) }
+    var tokens;
+    try { tokens = parse(re) }
     catch (err) { return false }
     
     var reps = 0;
+    /* The 'walk' function returns true if the node is safe, else false.
+     * The 'starHeight' value is incremented when REPETITION is encountered,
+     *   and this incremented value is then preserved in the recursive call to its stack/options. */
     return (function walk (node, starHeight) {
+        /* Sanity check: node is a 'ret node'. */
+        console.log('node type: ' + typeToString(node.type) + ', starHeight: ' + starHeight);
+        if (!isValidRetType(node.type)) {
+            console.log('Error, node doesn\'t look valid: type <' + node.type + '>');
+            console.log(retNodeToString(node));
+            return false;
+        }
+
         if (node.type === types.REPETITION) {
-            starHeight ++;
-            reps ++;
+            starHeight++;
+            reps++;
             if (starHeight > 1) return false;
             if (reps > replimit) return false;
         }
         
-        if (node.options) {
-            for (var i = 0, len = node.options.length; i < len; i++) {
-                var ok = walk({ stack: node.options[i] }, starHeight);
-                if (!ok) return false;
-            }
+        /* Walk each option for safety. */
+        var options = node.options || (node.value && node.value.options) || [];
+        for (var i = 0, len = options.length; i < len; i++) {
+            var ok = walk({ type: types.GROUP, stack: options[i] }, starHeight);
+            if (!ok) return false;
         }
-        var stack = node.stack || (node.value && node.value.stack);
-        if (!stack) return true;
-        
+
+        /* Walk each stack node for safety. */
+        var stack = node.stack || (node.value && node.value.stack) || [];
         for (var i = 0; i < stack.length; i++) {
+            console.log('Walking stack node ' + i + ':\n' + retNodeToString(stack[i]));
             var ok = walk(stack[i], starHeight);
             if (!ok) return false;
         }
         
+        /* Nothing left -- we're safe. */
         return true;
-    })(re, 0);
+    })(tokens, 0);
 };
 
 function isRegExp (x) {
     return {}.toString.call(x) === '[object RegExp]';
+}
+
+/* Return a string representation of a node from ret. */
+function retNodeToString (retNode) {
+	return JSON.stringify(retNode, null, 2);
+}
+
+/* Sanity check: Is this type one of the node types defined by ret? */
+function isValidRetType (type) {
+	for (var t in types) {
+		if (types[t] === type)
+			return true;
+	}
+	return false;
+}
+
+function typeToString (type) {
+	for (var t in types) {
+		if (types[t] === type)
+			return t;
+	}
+	return '';
 }
